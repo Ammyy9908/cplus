8b77b8a83ec20508a1ca23721186c695494ed011
ammyy9908 <sb78639@gmail.com>
2021-10-31 22:12:18 +0530

    types added

---
+++ types.cpp
@@ -0,0 +1,48 @@
+//
+//  types.cpp
+//  DSA
+//
+//  Created by Sumit kumar on 31/10/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    // char
+    char grade = 'A';
+    cout << grade << endl;
+    
+    // string
+    
+    string name = "Sumit Bighaniya";
+    
+    cout << name << endl;
+    
+    // numbers
+    //whole number or integers
+    int age = 25;
+    cout << age << endl;
+    
+    // floating numbers having decimal place
+    
+    double cgpa = 7.0;
+    
+    cout << cgpa << endl;
+    
+    // bool
+    
+    bool isRainy = false;
+    
+    if(isRainy){
+        cout << "Today is Rainy Day" << endl;
+    }
+    else{
+        cout << "It is sunny day" << endl;
+    }
+    
+    
+    
+    return 0;
+}
