e4d0854aac3a7c81bf3593d4b0a66605ca6c7f09
ammyy9908 <sb78639@gmail.com>
2021-10-31 23:00:57 +0530

    Numbers added' ' ' '

---
+++ Numbers.cpp
@@ -0,0 +1,37 @@
+//
+//  Numbers.cpp
+//  DSA
+//
+//  Created by Sumit kumar on 31/10/21.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int num1 = 10;
+    double num2 = 15.5;
+    cout << num1 << endl;
+    cout << num2 << endl;
+    num1++;
+    cout << num1 << endl;
+    
+    num2+=1;
+    cout << num2 << endl;
+    
+    // math operations
+    
+    cout << pow(2,5) << endl;
+    
+    cout << sqrt(16) << endl;
+    
+    cout << round(4.5) << endl;
+    
+    cout << floor(4.8) << endl;
+    
+    cout << fmax(3,10) << endl;
+    
+    cout << fmin(3,9) << endl;
+    return 0;
+}
---
+++ Numbers.hpp
@@ -0,0 +1,13 @@
+//
+//  Numbers.hpp
+//  DSA
+//
+//  Created by Sumit kumar on 31/10/21.
+//
+
+#ifndef Numbers_hpp
+#define Numbers_hpp
+
+#include <stdio.h>
+
+#endif /* Numbers_hpp */
